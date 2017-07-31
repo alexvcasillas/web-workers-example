037b1c0737e99ed94e6bd63ef58899d591d1aa4e
Alex Casillas <alexvcasillas@gmail.com>
2017-07-31 21:19:54 +0200

    Created a proper project

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
+const main = document.getElementById('main');
+const noSupport = document.getElementById('no-support');
+const taskSort = document.getElementById('task-sort');
+const taskAdd = document.getElementById('task-add');
+const taskResult = document.getElementById('task-result');
+const numberToAdd = document.getElementById('number-to-add');
+
 if (window.Worker) {
-  // Check if Browser supports the Worker api.
-  // Requires script name as input
+  noSupport.style.display = 'none';
+
   var myWorker = new Worker('worker.js');
 
-  myWorker.postMessage('Hello from the main');
+  taskSort.addEventListener('click', function(event) {
+    console.log('Sort Task Triggered');
+    myWorker.postMessage({ code: 'START_SORT' });
+  });
+
+  taskAdd.addEventListener('click', function(event) {
+    console.log('Add Task Triggered');
+    const number = numberToAdd.value;
+    myWorker.postMessage({ code: 'PUSH_NUMBER', number: number });
+  });
 
   myWorker.onmessage = function(e) {
-    console.log('Message received from worker: ', e.data);
+    const { data } = e;
+    console.log('Message received from worker: ', data);
+    switch (data.code) {
+      case 'NAN':
+        taskResult.classList.remove('success');
+        taskResult.classList.add('error');
+        taskResult.textContent = data.message;
+        break;
+      case 'SORT_END':
+        taskResult.classList.remove('error');
+        taskResult.classList.add('success');
+        taskResult.innerHTML = `
+				Initial Sort Time: ${data.initialSortTime}.<br>
+				End Sort Time: ${data.endSortTime}.
+				`.trim();
+        break;
+      case 'NUMBER_ADDED':
+        taskResult.classList.remove('error');
+        taskResult.classList.add('success');
+        taskResult.textContent = data.message;
+        break;
+    }
   };
+} else {
+  // This browser does not support Web Workers
+  main.style.display = 'none';
 }
--- worker.js
+++ worker.js
@@ -1,5 +1,57 @@
+let array = [];
+let initalSortTime, endSortTime;
+
+const fillArray = () => {
+  for (let i = 0; i < 100000; i++) {
+    array.push(parseInt(Math.random() * (100000 - 0) + 0, 10));
+  }
+  console.log('Filling the Array Task had just finished');
+};
+
+(function() {
+  console.log('Web Worker has been instantiated');
+  console.log('Time to fill the array');
+  fillArray();
+})();
+
+const sortCompare = (a, b) => {
+  return a - b;
+};
+
+const arraySort = () => {
+  initialSortTime = new Date();
+  array.sort(sortCompare);
+  endSortTime = new Date();
+  console.log(array);
+  postMessage({
+    code: 'SORT_END',
+    initialSortTime: initialSortTime,
+    endSortTime: endSortTime
+  });
+};
+
+const arrayAdd = number => {
+  const newNumber = parseInt(number, 10);
+  if (isNaN(newNumber))
+    return postMessage({
+      code: 'NAN',
+      message: 'There was an error while trying to parsing the given data.'
+    });
+  array.push(newNumber);
+  postMessage({
+    code: 'NUMBER_ADDED',
+    message: `Number ${newNumber} added to the array`
+  });
+};
+
 onmessage = function(e) {
-  console.log('Message received from main script');
-  console.log('Posting message back to main script');
-  postMessage('Hello from worker');
+  const { code, number } = e.data;
+  switch (code) {
+    case 'START_SORT':
+      arraySort();
+      break;
+    case 'PUSH_NUMBER':
+      arrayAdd(number);
+      break;
+  }
 };

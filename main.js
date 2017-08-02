5b90b09815a520ff47391003c70c23ba5f6100b2
Alex Casillas <alexvcasillas@gmail.com>
2017-08-03 00:22:17 +0200

    Finally working... Time to get some sleep :)

--- main.js
+++ main.js
@@ -1,48 +1,77 @@
+// DOM Objects to manipulate
 const main = document.getElementById('main');
 const noSupport = document.getElementById('no-support');
 const taskSort = document.getElementById('task-sort');
 const taskAdd = document.getElementById('task-add');
-const taskResult = document.getElementById('task-result');
+const sortResult = document.getElementById('sort-result');
+const addResult = document.getElementById('add-result');
 const numberToAdd = document.getElementById('number-to-add');
+const progress = document.getElementById('working');
 
+// Check if the browser supports Web Workers
 if (window.Worker) {
+  // Hide no support section
   noSupport.style.display = 'none';
-
+  // Instantiate the Web Worker
   var myWorker = new Worker('worker.js');
-
+  // Sort Task Button Listener
   taskSort.addEventListener('click', function(event) {
+    taskSort.classList.add('disabled');
     console.log('Sort Task Triggered');
     myWorker.postMessage({ code: 'START_SORT' });
   });
-
+  // Add Number Listener
   taskAdd.addEventListener('click', function(event) {
     console.log('Add Task Triggered');
-    const number = numberToAdd.value;
-    myWorker.postMessage({ code: 'PUSH_NUMBER', number: number });
+    const number =
+      numberToAdd.value === ''
+        ? parseInt(Math.random() * (100000 - 0) + 0, 10)
+        : numberToAdd.value;
+    myWorker.postMessage({ code: 'ADD_NUMBER', number: number });
   });
 
+  // Automatically add a number to the worker every second
+  const autoAddInterval = setInterval(function() {
+    myWorker.postMessage({
+      code: 'ADD_NUMBER',
+      number: parseInt(Math.random() * (100000 - 0) + 0, 10)
+    });
+  }, 1000);
+
+  // Here we handle the messages we got back from the worker
   myWorker.onmessage = function(e) {
     const { data } = e;
     console.log('Message received from worker: ', data);
     switch (data.code) {
       case 'NAN':
-        taskResult.classList.remove('success');
-        taskResult.classList.add('error');
-        taskResult.textContent = data.message;
+        addResult.classList.remove('success');
+        addResult.classList.add('error');
+        addResult.textContent = data.message;
+        break;
+      case 'SORT_START':
+        progress.classList.add('show');
+        sortResult.classList.remove('error');
+        sortResult.classList.add('success');
+        sortResult.textContent = data.message;
         break;
       case 'SORT_END':
-        taskResult.classList.remove('error');
-        taskResult.classList.add('success');
-        taskResult.innerHTML = `
-				Initial Sort Time: ${data.initialSortTime}.<br>
-				End Sort Time: ${data.endSortTime}.
-				`.trim();
+        progress.classList.remove('show');
+        taskSort.classList.remove('disabled');
+        clearInterval(autoAddInterval); // Clear the auto-add number interval :)
+        sortResult.classList.remove('error');
+        sortResult.classList.add('success');
+        addResult.style.display = 'none';
+        sortResult.innerHTML = `Time Spent sorting: ${data.spentTime} ms`.trim();
         break;
       case 'NUMBER_ADDED':
-        taskResult.classList.remove('error');
-        taskResult.classList.add('success');
-        taskResult.textContent = data.message;
+        addResult.classList.remove('error');
+        addResult.classList.add('success');
+        addResult.textContent = data.message;
         break;
+      case 'ADD_END':
+        addResult.classList.remove('error');
+        addResult.classList.add('success');
+        addResult.textContent = `Time Spent Adding number ${data.number}: ${data.spentTime} ms`.trim();
     }
   };
 } else {
--- worker.js
+++ worker.js
@@ -1,6 +1,11 @@
+// Array where values will be stored
 let array = [];
-let initalSortTime, endSortTime;
+// Flag which let us stop the sorting
+var stopSort = false;
+// Size of the chunks we're going to generate
+const chunkSize = 1000;
 
+// This method fills the array
 const fillArray = () => {
   for (let i = 0; i < 100000; i++) {
     array.push(parseInt(Math.random() * (100000 - 0) + 0, 10));
@@ -8,49 +13,109 @@ const fillArray = () => {
   console.log('Filling the Array Task had just finished');
 };
 
+// IIF to fill the array the moment the Web Worker is instantiated
 (function() {
   console.log('Web Worker has been instantiated');
   console.log('Time to fill the array');
   fillArray();
 })();
 
+// Function to handle the sort
 const sortCompare = (a, b) => {
   return a - b;
 };
 
-const arraySort = () => {
-  initialSortTime = new Date();
-  array.sort(sortCompare);
-  endSortTime = new Date();
-  console.log(array);
+// This is the function that does all the magic
+const asyncBubbleSort = () => {
+  // Send the client a message back telling the sort had just started
   postMessage({
-    code: 'SORT_END',
-    initialSortTime: initialSortTime,
-    endSortTime: endSortTime
+    code: 'SORT_START',
+    message: 'Started working on the array sort'
   });
+  // Get the initial sort time
+  const initialSortTime = new Date().getTime();
+  // Here we will be storing the chunks
+  let sortedArray = [];
+  // Amount of chunks processed
+  let chunksProcessed = 0;
+  // Generate an iterator based on chunks amount
+  const sortInterval = setInterval(function() {
+    if (stopSort) {
+      console.log('Sort is stopped. We should return and wait');
+      return;
+    }
+    // Splice from the origin array the amount of elements we want the chunk to have
+    let chunk = array.splice(0, chunkSize);
+    // If there's nothing to sort, let's end the sorting function
+    if (chunk.length === 0) {
+      // Get the end time of the sorting process
+      const endSortTime = new Date().getTime();
+      console.log('The chunk is empty. Add more numbers to the array');
+      console.log('Chunks processed: ', chunksProcessed);
+      console.log('Sorted Array: ', sortedArray);
+      // Clear the interval to stop the functionality
+      clearInterval(sortInterval);
+      /**
+       * Here we set the original array back again with the sorted array values
+       * Just in case we want to trigger again manually the sort button
+       */
+      array = sortedArray;
+      /**
+       * Send a message to the client telling the sort had just ended with 
+       * the values needed to be displayed at the UI.
+       */
+      postMessage({
+        code: 'SORT_END',
+        initialSortTime: initialSortTime,
+        endSortTime: endSortTime,
+        spentTime: endSortTime - initialSortTime
+      });
+      return;
+    }
+    // Push the chunk to the array
+    sortedArray.push(...chunk);
+    // Sort the array
+    sortedArray.sort(sortCompare);
+    console.log('Chunk added to the array: ', chunk);
+    chunksProcessed++;
+  }, 100);
 };
 
+// This function adds a new number to the array
 const arrayAdd = number => {
+  initialAddTime = new Date().getTime();
+  console.log(`Add number ${number} to the array`);
+  stopSort = true;
   const newNumber = parseInt(number, 10);
-  if (isNaN(newNumber))
+  if (isNaN(newNumber)) {
+    stopSort = false;
     return postMessage({
       code: 'NAN',
       message: 'There was an error while trying to parsing the given data.'
     });
+  }
   array.push(newNumber);
-  postMessage({
-    code: 'NUMBER_ADDED',
-    message: `Number ${newNumber} added to the array`
+  endAddTime = new Date().getTime();
+  stopSort = false;
+  return postMessage({
+    code: 'ADD_END',
+    number: newNumber,
+    initialAddTime: initialAddTime,
+    endAddTime: endAddTime,
+    spentTime: endAddTime - initialAddTime
   });
 };
 
+// Here we handle the messages received by the client
 onmessage = function(e) {
   const { code, number } = e.data;
   switch (code) {
     case 'START_SORT':
-      arraySort();
+      console.log('START_SORT cmd received.');
+      asyncBubbleSort();
       break;
-    case 'PUSH_NUMBER':
+    case 'ADD_NUMBER':
+      console.log('ADD_NUMBER cmd received');
       arrayAdd(number);
       break;
   }
